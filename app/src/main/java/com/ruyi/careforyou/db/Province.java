d86c2e179cfda70ff5d450f0250eb3b0818fd486
Mrwenshuai <1154355867@qq.com>
2019-05-06 17:31:49 +0800

    创建数据库和表的各项配置

---
+++ app/src/main/java/com/ruyi/careforyou/db/Province.java
@@ -0,0 +1,47 @@
+package com.ruyi.careforyou.db;
+
+import org.litepal.exceptions.DataSupportException;
+
+public class Province extends DataSupportException {
+
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+
+
+    public Province(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public Province(String errorMessage, Throwable throwable) {
+        super(errorMessage, throwable);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
